51f099e29df25f7d5b66dfe67f9fbb6433d51a67
Liran Tal <liran.tal@gmail.com>
2012-01-30 00:44:49 +0200

    cleaned up script

--- themes/admin/assets/js/script.js
+++ themes/admin/assets/js/script.js
@@ -24,26 +24,6 @@ $(function () {
 	// 	return false;
 	// });
 
-	// jQuery dataTables
-	// $('.datatable').dataTable(
-	// 	{
-	// 		"bProcessing": true,
-    //    	"bServerSide": true,
-    //    	"bAutoWidth":true,
-    //    	"sPaginationType": "full_numbers",
-    //    	"sAjaxSource": "http://localhost:8080/services/strategy/manage/get_all_strategies/1",
-    //    	"fnServerData": function ( sSource, aoData, fnCallback ) {
-	//             $.ajax( {
-	//                 "dataType": 'json', 
-	//                 "type": "POST", 
-	//                 "url": sSource, 
-	//                 "data": aoData, 
-	//                 "success": fnCallback
-	//             } );
-    //    	}
-	// 	}
-	// );
-
 	// jQuery Custome File Input
 	$('.fileupload').customFileInput();
 
