67ba55dc06589c77f18e355ab8e677430a298d32
Liran Tal <liran.tal@gmail.com>
2012-02-06 19:43:25 +0200

    added support for pushing statistics links from other modules (microdeal etc

--- application/modules/strategy/views/reports/strategy_reports_links.php
+++ application/modules/strategy/views/reports/strategy_reports_links.php
@@ -5,8 +5,18 @@
 		$link_1 = base_url() . 'strategy/reports/demographics/index/'.$strategy['id'].'/'.$campaign['id'];
 		$link_2 = base_url() . 'strategy/reports/mobile/index/'.$strategy['id'].'/'.$campaign['id'];
 	}
-?>
 
-	<li><a href="<?php echo isset($link_1) ? $link_1 : '#'?>" rel="tooltip" title="Switch to next tab">Demographics</a></li>
+	$data['strategy'] = isset($strategy) ? $strategy : '';
+	$data['campaign'] = isset($campaign) ? $campaign : '';
+	$data['plan'] = isset($plan) ? $plan : '';
+
+    // we need to know which stratey type is this so we can fwd to the correct module
+    if (isset($strategy['strategy_type_name']))
+    {
+        $strategy_type = $strategy['strategy_type_name'];
+        echo Modules::run($strategy_type.'/microsite_reports_overview/_get_reports_links', $data);
+    }
+
+?>
 
 	<li><a href="<?php echo isset($link_2) ? $link_2 : '#'?>" rel="tooltip" title="Switch to next tab">Mobile</a></li>
