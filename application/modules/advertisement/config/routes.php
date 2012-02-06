bf70b6721d7132654b2c2e10e1c9f6f4d01c9082
Liran Tal <liran.tal@gmail.com>
2012-02-06 19:41:35 +0200

    minor updates on code and changed routes

--- application/modules/advertisement/config/routes.php
+++ application/modules/advertisement/config/routes.php
@@ -2,4 +2,4 @@
 
 // set default page for users/ module
 $route['advertisement'] = "advertisement_manage/index";
-$route['advertisement/(:any)?'] = "advertisement_manage/$1";
+$route['advertisement/manage/(:any)?'] = "advertisement_manage/$1";
