77122ab6fe14bddf403ef9695f1ddba66fa99f04
Liran Tal <liran.tal@gmail.com>
2012-08-13 21:55:41 +0300

    updated create new campaign code

--- application/views/create_new_campaign.php
+++ application/views/create_new_campaign.php
@@ -1,9 +1,316 @@
+<?php
+/*
 <iframe allowtransparency="true" src="http://form.jotform.me/form/22077395771461" frameborder="0" style="width:100%; height:519px; border:none;" scrolling="no">
 </iframe>
 
-<?php
-/*
-or we can also utilize the script
+<!-- or we can also utilize the script
 <script type="text/javascript" src="http://form.jotform.com/jsform/13024216493"></script>
+-->
 */
 ?>
+
+<script src="http://max.jotfor.ms/min/g=jotform?3.0.3783" type="text/javascript"></script>
+<script type="text/javascript">
+   JotForm.init(function(){
+      JotForm.description('input_9', '300x300 pixles');
+      $('input_10').hint('  e.g. \"Get a free burger!\"');
+      $('input_11').hint('  e.g., \"I just got a deal for a free burger.. You can too!');
+      JotForm.alterTexts({"alphabetic":"This field can only contain letters","alphanumeric":"This field can only contain letters and numbers.","confirmClearForm":"Are you sure you want to clear the form?","email":"Enter a valid e-mail address","incompleteFields":"There are incomplete required fields. Please complete them.","lessThan":"Your score should be less than","numeric":"This field can only contain numeric values","pleaseWait":"Please wait...","required":"This field is required.","uploadExtensions":"You can only upload following files:","uploadFilesize":"File size cannot be bigger than:"});
+   });
+</script>
+<link href="http://max.jotfor.ms/min/g=formCss?3.0.3783" rel="stylesheet" type="text/css" />
+<link type="text/css" rel="stylesheet" href="http://jotform.me/css/styles/jottheme.css?3.0.3783" />
+<style type="text/css">
+    .form-label{
+        width:160px !important;
+    }
+    .form-label-left{
+        width:160px !important;
+    }
+    .form-line{
+        padding-top:10px;
+        padding-bottom:10px;
+    }
+    .form-label-right{
+        width:160px !important;
+    }
+    .form-all{
+        width:850px;
+        background:#FFFFFF;
+        color:#666465 !important;
+        font-family:'Arial';
+        font-size:15px;
+    }
+    .form-radio-item label, .form-checkbox-item label, .form-grading-label, .form-header{
+        color:#666465;
+    }
+
+    /* Injected CSS Code */
+.qq-upload-button {
+background: #b8573d!important;
+color: #ffffff!important;
+text-shadow: 2 1px #ffffff!important;  }.form-submit-button {
+background: #b8573d!important;
+color: #ffffff!important;
+text-shadow: 1 -1px #ffffff!important;  }.form-submit-reset{
+background: #b8573d!important;
+color: #ffffff!important;
+text-shadow: 1 -1px #ffffff!important;  }.form-submit-print {
+background: #b8573d!important;
+color: #b8573d!important;
+text-shadow: 1 -1px #ffffff!important;  }.form-screen-button {
+background: #b8573d!important;
+color: #b8573d!important;
+text-shadow: 0 -1px #ffffff!important;  }.form-all {
+background-color: #FFFFFF;
+border: 1px solid gray;
+border-radius: 3px 3px 3px 3px;
+margin-top: 5px;
+padding-bottom: 2px;
+}
+.form-textbox{ width: ; height: 22px; }.form-dropdown{ width: ; height: 22px; }.form-label-left{ width: ; height: 22px; }
+    /* Injected CSS Code */
+</style>
+
+<link type="text/css" rel="stylesheet" href="http://jotform.me/css/styles/buttons/form-submit-button-cool_grey.css?3.0.3783"/>
+<form class="jotform-form" action="http://submit.jotform.me/submit/22077395771461/" method="post" enctype="multipart/form-data" name="form_22077395771461" id="22077395771461" accept-charset="utf-8">
+  <input type="hidden" name="formID" value="22077395771461" />
+  <div class="form-all">
+    <ul class="form-section">
+      <li id="cid_17" class="form-input-wide">
+        <div class="form-header-group">
+          <h1 id="header_17" class="form-header">
+            Create new campaign
+          </h1>
+        </div>
+      </li>
+      <li class="form-line form-line-column" id="id_9">
+        <label class="form-label-top" id="label_9" for="input_9">
+          Deal image<span class="form-required">*</span>
+        </label>
+        <div id="cid_9" class="form-input-wide"><span class="form-sub-label-container"><div class="qq-uploader-buttonText-value">
+              Upload a File
+            </div>
+            <input class="form-upload-multiple validate[required]" type="file" id="input_9" name="q9_dealImage9[]" multiple="multiple" file-accept="jpg, jpeg, png, gif" file-maxsize="1024" />
+            <label class="form-sub-label" for="input_9"> These will be part of both your mobile campaign and you social media promotion </label></span>
+        </div>
+      </li>
+      <li class="form-line form-line-column" id="id_10">
+        <label class="form-label-top" id="label_10" for="input_10">
+          Deal headline<span class="form-required">*</span>
+        </label>
+        <div id="cid_10" class="form-input-wide">
+          <input type="text" class="form-textbox validate[required]" id="input_10" name="q10_dealHeadline" size="30" maxlength="64" />
+        </div>
+      </li>
+      <li class="form-line" id="id_11">
+        <label class="form-label-top" id="label_11" for="input_11"> Personalized social media promotion message </label>
+        <div id="cid_11" class="form-input-wide"><span class="form-sub-label-container"><input type="text" class="form-textbox" id="input_11" name="q11_personalizedSocial11" size="90" maxlength="512" />
+            <label class="form-sub-label" for="input_11"> This will be the body of your social media promotion message (Wall-Post / Tweet) </label></span>
+        </div>
+      </li>
+      <li class="form-line" id="id_12">
+        <label class="form-label-left" id="label_12" for="input_12"> URL </label>
+        <div id="cid_12" class="form-input"><span class="form-sub-label-container"><input type="text" class="form-textbox" id="input_12" name="q12_url" size="20" />
+            <label class="form-sub-label" for="input_12"> e.g., Your brand's website URL, Your brand's Facebook page / Tweeter, etc. </label></span>
+        </div>
+      </li>
+      <li class="form-line" id="id_13">
+        <label class="form-label-left" id="label_13" for="input_13">
+          Deal bank size<span class="form-required">*</span>
+        </label>
+        <div id="cid_13" class="form-input"><span class="form-sub-label-container"><select class="form-dropdown validate[required]" style="width:150px" id="input_13" name="q13_dealBank">
+              <option>  </option>
+              <option value="100 Deals"> 100 Deals </option>
+              <option value="250 Deals"> 250 Deals </option>
+              <option value="500 Deals"> 500 Deals </option>
+              <option value="1,000 Deals"> 1,000 Deals </option>
+              <option value="10,000 Deals"> 10,000 Deals </option>
+              <option value="50,000 Deals!"> 50,000 Deals! </option>
+              <option value="100,000 Deals!"> 100,000 Deals! </option>
+            </select>
+            <label class="form-sub-label" for="input_13"> This is the total number of deals you wish to purchase. Remember, deal = Facebook promotion </label></span>
+        </div>
+      </li>
+      <li class="form-line" id="id_16">
+        <label class="form-label-left" id="label_16" for="input_16">
+          Campaign exipration<span class="form-required">*</span>
+        </label>
+        <div id="cid_16" class="form-input"><span class="form-sub-label-container"><select class="form-dropdown validate[required]" name="q16_campaignExipration16[day]" id="input_16_day">
+              <option>  </option>
+              <option value="1"> 1 </option>
+              <option value="2"> 2 </option>
+              <option value="3"> 3 </option>
+              <option value="4"> 4 </option>
+              <option value="5"> 5 </option>
+              <option value="6"> 6 </option>
+              <option value="7"> 7 </option>
+              <option value="8"> 8 </option>
+              <option value="9"> 9 </option>
+              <option value="10"> 10 </option>
+              <option value="11"> 11 </option>
+              <option value="12"> 12 </option>
+              <option value="13"> 13 </option>
+              <option value="14"> 14 </option>
+              <option value="15"> 15 </option>
+              <option value="16"> 16 </option>
+              <option value="17"> 17 </option>
+              <option value="18"> 18 </option>
+              <option value="19"> 19 </option>
+              <option value="20"> 20 </option>
+              <option value="21"> 21 </option>
+              <option value="22"> 22 </option>
+              <option value="23"> 23 </option>
+              <option value="24"> 24 </option>
+              <option value="25"> 25 </option>
+              <option value="26"> 26 </option>
+              <option value="27"> 27 </option>
+              <option value="28"> 28 </option>
+              <option value="29"> 29 </option>
+              <option value="30"> 30 </option>
+              <option value="31"> 31 </option>
+            </select>
+            <label class="form-sub-label" for="input_16_day" id="sublabel_day"> Day </label></span><span class="form-sub-label-container"><select class="form-dropdown validate[required]" name="q16_campaignExipration16[month]" id="input_16_month">
+              <option>  </option>
+              <option value="January"> January </option>
+              <option value="February"> February </option>
+              <option value="March"> March </option>
+              <option value="April"> April </option>
+              <option value="May"> May </option>
+              <option value="June"> June </option>
+              <option value="July"> July </option>
+              <option value="August"> August </option>
+              <option value="September"> September </option>
+              <option value="October"> October </option>
+              <option value="November"> November </option>
+              <option value="December"> December </option>
+            </select>
+            <label class="form-sub-label" for="input_16_month" id="sublabel_month"> Month </label></span><span class="form-sub-label-container"><select class="form-dropdown validate[required]" name="q16_campaignExipration16[year]" id="input_16_year">
+              <option>  </option>
+              <option value="2016"> 2016 </option>
+              <option value="2015"> 2015 </option>
+              <option value="2014"> 2014 </option>
+              <option value="2013"> 2013 </option>
+              <option value="2012"> 2012 </option>
+              <option value="2011"> 2011 </option>
+              <option value="2010"> 2010 </option>
+              <option value="2009"> 2009 </option>
+              <option value="2008"> 2008 </option>
+              <option value="2007"> 2007 </option>
+              <option value="2006"> 2006 </option>
+              <option value="2005"> 2005 </option>
+              <option value="2004"> 2004 </option>
+              <option value="2003"> 2003 </option>
+              <option value="2002"> 2002 </option>
+              <option value="2001"> 2001 </option>
+              <option value="2000"> 2000 </option>
+              <option value="1999"> 1999 </option>
+              <option value="1998"> 1998 </option>
+              <option value="1997"> 1997 </option>
+              <option value="1996"> 1996 </option>
+              <option value="1995"> 1995 </option>
+              <option value="1994"> 1994 </option>
+              <option value="1993"> 1993 </option>
+              <option value="1992"> 1992 </option>
+              <option value="1991"> 1991 </option>
+              <option value="1990"> 1990 </option>
+              <option value="1989"> 1989 </option>
+              <option value="1988"> 1988 </option>
+              <option value="1987"> 1987 </option>
+              <option value="1986"> 1986 </option>
+              <option value="1985"> 1985 </option>
+              <option value="1984"> 1984 </option>
+              <option value="1983"> 1983 </option>
+              <option value="1982"> 1982 </option>
+              <option value="1981"> 1981 </option>
+              <option value="1980"> 1980 </option>
+              <option value="1979"> 1979 </option>
+              <option value="1978"> 1978 </option>
+              <option value="1977"> 1977 </option>
+              <option value="1976"> 1976 </option>
+              <option value="1975"> 1975 </option>
+              <option value="1974"> 1974 </option>
+              <option value="1973"> 1973 </option>
+              <option value="1972"> 1972 </option>
+              <option value="1971"> 1971 </option>
+              <option value="1970"> 1970 </option>
+              <option value="1969"> 1969 </option>
+              <option value="1968"> 1968 </option>
+              <option value="1967"> 1967 </option>
+              <option value="1966"> 1966 </option>
+              <option value="1965"> 1965 </option>
+              <option value="1964"> 1964 </option>
+              <option value="1963"> 1963 </option>
+              <option value="1962"> 1962 </option>
+              <option value="1961"> 1961 </option>
+              <option value="1960"> 1960 </option>
+              <option value="1959"> 1959 </option>
+              <option value="1958"> 1958 </option>
+              <option value="1957"> 1957 </option>
+              <option value="1956"> 1956 </option>
+              <option value="1955"> 1955 </option>
+              <option value="1954"> 1954 </option>
+              <option value="1953"> 1953 </option>
+              <option value="1952"> 1952 </option>
+              <option value="1951"> 1951 </option>
+              <option value="1950"> 1950 </option>
+              <option value="1949"> 1949 </option>
+              <option value="1948"> 1948 </option>
+              <option value="1947"> 1947 </option>
+              <option value="1946"> 1946 </option>
+              <option value="1945"> 1945 </option>
+              <option value="1944"> 1944 </option>
+              <option value="1943"> 1943 </option>
+              <option value="1942"> 1942 </option>
+              <option value="1941"> 1941 </option>
+              <option value="1940"> 1940 </option>
+              <option value="1939"> 1939 </option>
+              <option value="1938"> 1938 </option>
+              <option value="1937"> 1937 </option>
+              <option value="1936"> 1936 </option>
+              <option value="1935"> 1935 </option>
+              <option value="1934"> 1934 </option>
+              <option value="1933"> 1933 </option>
+              <option value="1932"> 1932 </option>
+              <option value="1931"> 1931 </option>
+              <option value="1930"> 1930 </option>
+              <option value="1929"> 1929 </option>
+              <option value="1928"> 1928 </option>
+              <option value="1927"> 1927 </option>
+              <option value="1926"> 1926 </option>
+              <option value="1925"> 1925 </option>
+              <option value="1924"> 1924 </option>
+              <option value="1923"> 1923 </option>
+              <option value="1922"> 1922 </option>
+              <option value="1921"> 1921 </option>
+              <option value="1920"> 1920 </option>
+            </select>
+            <label class="form-sub-label" for="input_16_year" id="sublabel_year"> Year </label></span>
+        </div>
+      </li>
+      <li class="form-line" id="id_2">
+        <div id="cid_2" class="form-input-wide">
+          <div style="text-align:left" class="form-buttons-wrapper">
+            <button id="input_2" type="submit" class="form-submit-button form-submit-button-cool_grey">
+              Send
+            </button>
+            &nbsp;
+            <button id="input_reset_2" type="reset" class="form-submit-reset form-submit-button-cool_grey">
+              Clear Form
+            </button>
+          </div>
+        </div>
+      </li>
+      <li style="display:none">
+        Should be Empty:
+        <input type="text" name="website" value="" />
+      </li>
+    </ul>
+  </div>
+  <input type="hidden" id="simple_spc" name="simple_spc" value="22077395771461" />
+  <script type="text/javascript">
+  document.getElementById("si" + "mple" + "_spc").value = "22077395771461-22077395771461";
+  </script>
+</form>
+<?=	theme_js('iColorPicker.js'); ?>
+<input id="mycolor" name="mycolor" type="text" value="#ffcc00" class="iColorPicker" />
